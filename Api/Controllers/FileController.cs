bb6ddad713b033c0efce44da0f4f98ed0059a6c4
CollapseNav <CollapseNav@163.com>
2020-11-08 00:47:45 +0800

    重新封装并且加了log

--- Api/Controllers/FileController.cs
+++ Api/Controllers/FileController.cs
@@ -11,6 +11,8 @@ using Api.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
 
 namespace Api.Controller
 {
@@ -19,8 +21,10 @@ namespace Api.Controller
     {
         private readonly FileServConfig _config;
         private readonly FileDbContext _context;
-        public FileController(FileServConfig config, FileDbContext dbContext)
+        private readonly ILogger _log;
+        public FileController(ILogger<FileController> logger, FileServConfig config, FileDbContext dbContext)
         {
+            _log = logger;
             _config = config;
             _context = dbContext;
         }
@@ -29,34 +33,31 @@ namespace Api.Controller
         [HttpPost("PostFiles")]
         public async Task<ReturnFileMapPath> PostFiles(IFormFile[] files)
         {
-            var fileIds = new List<Guid>();
+            var fileIds = new List<string>();
             foreach (var file in files)
-            {
-                string fileName = "/" + Guid.NewGuid().ToString() + "." + file.FileName.Split(".")[^1];
-                using FileStream fs = new FileStream(_config.FullPath + fileName, FileMode.CreateNew);
-                await file.CopyToAsync(fs);
-                var mod = new Model.File
-                {
-                    AddTime = DateTime.Now,
-                    Ext = file.FileName.Split(".")[^1],
-                    FileName = file.FileName,
-                    MapPath = _config.FrontExt + fileName,
-                    Id = Guid.NewGuid(),
-                };
-                _context.Files.Add(mod);
-                fileIds.Add(mod.Id.Value);
-                _context.SaveChanges();
-            }
+                fileIds.Add(await SaveFile(file));
             return new ReturnFileMapPath { Total = fileIds.Count, FileId = fileIds };
         }
 
         [DisableRequestSizeLimit]
         [HttpPost("PostSingleFile")]
-        public async Task<Guid> PostFile(IFormFile file)
+        public async Task<string> PostFile(IFormFile file)
         {
-            string fileName = "/" + Guid.NewGuid().ToString() + "." + file.FileName.Split(".")[^1];
-            using FileStream fs = new FileStream(_config.FullPath + fileName, FileMode.CreateNew);
-            await file.CopyToAsync(fs);
+            return await SaveFile(file);
+        }
+
+        private async Task<string> SaveFile(IFormFile file)
+        {
+            string fileName = GetFileName(file.FileName);
+            try
+            {
+                using FileStream fs = new FileStream(fileName, FileMode.CreateNew);
+                await file.CopyToAsync(fs);
+            }
+            catch (IOException)
+            {
+                throw new IOException("文件上传失败,请稍候重试.");
+            }
             var mod = new Model.File
             {
                 AddTime = DateTime.Now,
@@ -67,7 +68,24 @@ namespace Api.Controller
             };
             _context.Files.Add(mod);
             _context.SaveChanges();
-            return mod.Id.Value;
+            _log.LogInformation($"Save File {file.FileName} , size: {file.Length}; type: {file.FileName.Split(".")[^1]};");
+            return mod.Id.ToString();
+        }
+
+        private string GetFileName(string fileName)
+        {
+            var filePath = _config.FullPath + "/";
+            string fileExt = "." + fileName.Split(".")[^1];
+            if (_config.UseRawName)
+            {
+                filePath += fileName.Split(".")[0..^1].Join("");
+                if (System.IO.File.Exists(filePath + fileExt))
+                    filePath += "_" + DateTime.Now.Millisecond;
+            }
+            else
+                filePath += Guid.NewGuid().ToString();
+            filePath += fileExt;
+            return filePath;
         }
 
         [HttpGet("GetFile")]
@@ -79,6 +97,7 @@ namespace Api.Controller
             using var stream = new FileStream(filepath, FileMode.Open);
             await stream.CopyToAsync(memoryStream);
             memoryStream.Seek(0, SeekOrigin.Begin);
+            _log.LogInformation($"Download File {file.FileName} , Id : {id.Value}");
             Response.Headers.Add("Content-Disposition", "attachment; filename=" + HttpUtility.UrlEncode(file.FileName, Encoding.UTF8));
             return new FileStreamResult(memoryStream, "application/octet-stream");
         }
--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM dotnet/asp:3.1 AS base
+# FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS base
 WORKDIR /app
 EXPOSE 5000
 ENV ASPNETCORE_URLS http://+:5000
 ENV TZ=Asia/Shanghai
 
 FROM dotnet/sdk:3.1 AS publish
+# FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS publish
 WORKDIR /src
 COPY ./ ./
 RUN  dotnet publish Api -c Release -o /app
+COPY ./Api/Data.db /app/Data.db
 
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app .
-COPY ./Api/Data.db /app/Data.db
 ENTRYPOINT ["dotnet", "Api.dll"]
